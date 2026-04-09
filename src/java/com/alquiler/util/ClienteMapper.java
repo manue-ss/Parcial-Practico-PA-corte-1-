f250de80d6acd1402877763f7fdc2db935f81123
Manuel <juanmasalazarsalazar@gmail.com>
2026-04-09 02:06:04 -0500

    Añadir clase ClienteMapper para convertir entre Cliente y ClienteDTO

---
+++ src/java/com/alquiler/util/ClienteMapper.java
@@ -0,0 +1,46 @@
+package com.alquiler.util;
+
+import com.alquiler.model.dto.ClienteDTO;
+import com.alquiler.model.entities.Cliente;
+import com.alquiler.model.enums.TipoMembresia;
+
+/**
+ *
+ * @author Acer-Pc
+ */
+public class ClienteMapper {
+
+    public static Cliente toEntity(ClienteDTO clienteDTO) {
+        Cliente cliente = new Cliente();
+
+        cliente.setNombreCompleto(clienteDTO.getNombreCompleto());
+        cliente.setNombreUsuario(clienteDTO.getNombreUsuario());
+        cliente.setCorreo(clienteDTO.getCorreo());
+        cliente.setId(clienteDTO.getId());
+        cliente.setTelefono(clienteDTO.getTelefono());
+        cliente.setSaldo(clienteDTO.getSaldo());
+        TipoMembresia membresia = TipoMembresia.valueOf(clienteDTO.getMembresiaSeleccionada());
+        cliente.setMembresia(membresia);
+        cliente.setFechaPagoMembresia(clienteDTO.getFechaPagoMembresia());
+        cliente.setContrasenia(clienteDTO.getContrasenia());
+
+        return cliente;
+    }
+
+    public static ClienteDTO toDTO(Cliente cliente) {
+        ClienteDTO clienteDTO = new ClienteDTO();
+
+        clienteDTO.setNombreCompleto(cliente.getNombreCompleto());
+        clienteDTO.setNombreUsuario(cliente.getNombreUsuario());
+        clienteDTO.setCorreo(cliente.getCorreo());
+        clienteDTO.setId(cliente.getId());
+        clienteDTO.setTelefono(cliente.getTelefono());
+        clienteDTO.setSaldo(cliente.getSaldo());
+        String membresia = cliente.getMembresia().toString();
+        clienteDTO.setMembresiaSeleccionada(membresia);
+        clienteDTO.setFechaPagoMembresia(cliente.getFechaPagoMembresia());
+        clienteDTO.setContrasenia(cliente.getContrasenia());
+
+        return clienteDTO;
+    }
+}
