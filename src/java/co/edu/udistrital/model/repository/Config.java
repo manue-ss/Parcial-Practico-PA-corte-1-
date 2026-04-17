2bc3f0c1fee1270d827bdb4b7d391ee07509f3ba
Manuel <juanmasalazarsalazar@gmail.com>
2026-04-16 23:53:05 -0500

    feat: implementar utilidades de migración de bases de datos e integración con MariaDB.

---
+++ src/java/co/edu/udistrital/model/repository/Config.java
@@ -0,0 +1,8 @@
+package co.edu.udistrital.model.repository;
+
+public class Config {
+    public static final String URL = "jdbc:mariadb://localhost:3306/rentzone_db";
+    public static final String USER = "rentzone_user";
+    public static final String PASS = "rentzone123";
+    public static final String DRIVER = "org.mariadb.jdbc.Driver";
+}
